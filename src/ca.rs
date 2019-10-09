2edafa7e41640d0a9f28bf51190c8f224544b96a
Heiko Schaefer <heiko@schaefer.name>
2019-10-09 12:12:42 +0200

    add GPL header

--- src/ca.rs
+++ src/ca.rs
@@ -1,3 +1,20 @@
+// Copyright 2019 Heiko Schaefer heiko@schaefer.name
+//
+// This file is part of OpenPGP CA.
+//
+// OpenPGP CA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPGP CA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPGP CA.  If not, see <https://www.gnu.org/licenses/>.
+
 use failure::{self, ResultExt};
 
 use sequoia_openpgp as openpgp;
