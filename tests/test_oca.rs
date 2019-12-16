a129defe72eca11dd829d2799f56433303321734
Heiko Schaefer <heiko@schaefer.name>
2019-12-16 14:51:00 +0000

    test: creating two users with the same email address should fail without inserting a new user into the database

--- tests/test_oca.rs
+++ tests/test_oca.rs
@@ -40,3 +40,38 @@ fn test_ca() {
         println!("{}", user.pub_key);
     }
 }
+
+
+#[test]
+fn test_ca_insert_duplicate_email() {
+    let ctx = make_context!();
+
+    let home_path = String::from(ctx.get_homedir().to_str().unwrap());
+    let db = format!("{}/ca.sqlite", home_path);
+
+    let mut ca = ca::Ca::new(Some(&db));
+
+    // make new CA key
+    assert!(ca.ca_new(&["ca@example.org"]).is_ok());
+
+
+    // make CA user
+    let res = ca.user_new(Some(&"Alice"), Some(&["alice@example.org"]));
+    assert!(res.is_ok());
+
+    // make CA user with the same email address
+    let res = ca.user_new(Some(&"Alice"), Some(&["alice@example.org"]));
+    assert!(!res.is_ok());
+
+    let users = ca.get_users();
+    let users = users.unwrap();
+
+    assert_eq!(users.len(), 1);
+
+    println!("===================================================");
+
+    for user in users {
+        println!("user: {:?}", user.name);
+        println!("{}", user.pub_key);
+    }
+}
